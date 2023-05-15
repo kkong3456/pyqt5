0155fbdbe6cae4baf739cd7670842f29afaeac8c
unknown <kkong3456@gmail.com>
2023-05-16 06:26:37 +0900

    6-1 compltete

---
+++ 5-19_textbrowser.py
@@ -0,0 +1,43 @@
+import sys 
+from PyQt5.QtWidgets import (QApplication,QWidget,QLineEdit,QTextBrowser
+                             ,QPushButton,QVBoxLayout)
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.le=QLineEdit()
+        self.le.returnPressed.connect(self.append_text)
+        
+        self.tb=QTextBrowser()
+        self.tb.setAcceptRichText(True)
+        self.tb.setOpenExternalLinks(True)
+        
+        self.clear_btn=QPushButton('Clear')
+        self.clear_btn.pressed.connect(self.clear_text)
+        
+        vbox=QVBoxLayout()
+        vbox.addWidget(self.le)
+        vbox.addWidget(self.tb)
+        vbox.addWidget(self.clear_btn)
+        
+        self.setLayout(vbox)
+        
+        self.setWindowTitle('QTextBrowser')
+        self.setGeometry(300,300,300,300)
+        self.show()
+        
+    def append_text(self):
+        text=self.le.text()
+        self.tb.append(text)
+        self.le.clear()
+        
+    def clear_text(self):
+        self.tb.clear()
+        
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    ex=MyApp()
+    sys.exit(app.exec_())
